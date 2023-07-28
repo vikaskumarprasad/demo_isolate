b0d32d45532bee2d035d8cb1648ad50c53bd24bf
Vikaskumar Prasad <108278336+vikaspp@users.noreply.github.com>
2023-07-28 16:09:46 +0530

    Initial commit

---
+++ lib/src/home/home_view.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    const testStyle = TextStyle(
+      fontSize: 24,
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+    );
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6871F),
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 22, 0),
+            child: IconButton(
+              onPressed: () {
+                // showCustomDialog(context);
+
+                showCustomDialog1(context: context);
+              },
+              icon: const Icon(Icons.notifications),
+            ),
+          )
+        ],
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.light,
+        ),
+        backgroundColor: const Color(0xFFF6871F),
+        centerTitle: true,
+        title: const Text(
+          "Dashboard",
+          style: testStyle,
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: const Column(
+          children: [],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet<void>(
+            // barrierColor: Colors.deepPurpleAccent,
+
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                height: MediaQuery.of(context).size.height / 2,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text('Modal BottomSheet'),
+                      ElevatedButton(
+                        child: const Text('Close BottomSheet'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+showCustomDialog1({required BuildContext context}) {
+  return showGeneralDialog(
+    barrierLabel: "Label",
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.5),
+    transitionDuration: const Duration(milliseconds: 700),
+    context: context,
+    pageBuilder: (context, anim1, anim2) {
+      return Align(
+        alignment: Alignment.topCenter,
+        child: Material(
+          type: MaterialType.transparency,
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 2,
+                width: MediaQuery.of(context).size.width,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                    ),
+                  ),
+                  child: const Column(
+                    children: [
+                      Text("data"),
+                    ],
+                  ),
+                ),
+              ),
+              IconButton.filledTonal(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.close),
+              )
+              // InkWell(
+              //   onTap: () => Navigator.of(context).pop(),
+              //   child: Container(
+              //     height: 10,
+              //     width: 100,
+              //     decoration: BoxDecoration(
+              //       color: Colors.grey,
+              //       borderRadius: BorderRadius.circular(30),
+              //     ),
+              //   ),
+              // )
+            ],
+          ),
+        ),
+      );
+    },
+    transitionBuilder: (context, anim1, anim2, child) {
+      return SlideTransition(
+        position: Tween(
+          begin: const Offset(0, -1),
+          end: const Offset(0, 0),
+        ).animate(anim1),
+        child: child,
+      );
+    },
+  );
+}
+
+void showStatusBar(BuildContext context) {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle());
+}
+
+void showCustomDialog(BuildContext context) {
+  showGeneralDialog(
+    context: context,
+    barrierLabel: "Barrier",
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.5),
+    transitionDuration: const Duration(milliseconds: 700),
+    pageBuilder: (_, __, ___) {
+      return Center(
+        child: Container(
+          height: 240,
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: const SizedBox.expand(
+            child: FlutterLogo(),
+          ),
+        ),
+      );
+    },
+    transitionBuilder: (_, anim, __, child) {
+      Tween<Offset> tween;
+      if (anim.status == AnimationStatus.reverse) {
+        tween = Tween(begin: const Offset(-1, 0), end: Offset.zero);
+      } else {
+        tween = Tween(begin: const Offset(1, 0), end: Offset.zero);
+      }
+      return SlideTransition(
+        position: tween.animate(anim),
+        child: FadeTransition(
+          opacity: anim,
+          child: child,
+        ),
+      );
+    },
+  );
+}
